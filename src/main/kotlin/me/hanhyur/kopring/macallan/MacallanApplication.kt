ccb182586123dfd118fbcd9376bb800027fdd094
hanhyur <zeidgame@gmail.com>
2025-07-29 18:56:53 +0900

    chore : 프로젝트 생성
    <description>
    - 기본 프로젝트 생성

---
+++ src/main/kotlin/me/hanhyur/kopring/macallan/MacallanApplication.kt
@@ -0,0 +1,11 @@
+package me.hanhyur.kopring.macallan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MacallanApplication
+
+fun main(args: Array<String>) {
+    runApplication<MacallanApplication>(*args)
+}
---
+++ src/test/kotlin/me/hanhyur/kopring/macallan/MacallanApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.hanhyur.kopring.macallan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MacallanApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
